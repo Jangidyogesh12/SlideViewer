e27f3f010082c7cf2d65e91213a2ec1d9cab25dc
Jangidyogesh12 <jangidyogesh123@gmail.com>
2026-04-30 22:29:45 +0530

    fix : react compiler fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import "./App.css";
 import { slidesData } from "./slides";
-import { SlidePreview } from "./components/ui/SlidePreviw";
+// import { SlidePreview } from "./components/ui/SlidePreviw";
+import { SlidePreview } from "@yogesharma2003/slide-preview";
 
 function App() {
-  return <SlidePreview items={slidesData} theme="dark" />;
+  return <SlidePreview items={slidesData} theme="light" />;
 }
 
 export default App;
